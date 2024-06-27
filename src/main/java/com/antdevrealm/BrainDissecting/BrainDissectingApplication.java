5ec251396c18462d4eaffaeb21cfee2b38e487bd
AntoanYosifov <tapaktapxaomi@gmail.com>
2024-06-27 17:58:29 +0200

    Initial commit

---
+++ src/main/java/com/antdevrealm/BrainDissecting/BrainDissectingApplication.java
@@ -0,0 +1,13 @@
+package com.antdevrealm.BrainDissecting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainDissectingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainDissectingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antdevrealm/BrainDissecting/BrainDissectingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antdevrealm.BrainDissecting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainDissectingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
