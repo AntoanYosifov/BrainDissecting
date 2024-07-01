00f41b476ffb7087140111ca3e8bf17242b2d72b
AntoanYosifov <tapaktapxaomi@gmail.com>
2024-07-01 10:16:06 +0200

    Add @CrossOrigin annotation

--- src/main/java/com/antdevrealm/BrainDissecting/web/ArticleRestController.java
+++ src/main/java/com/antdevrealm/BrainDissecting/web/ArticleRestController.java
@@ -3,16 +3,14 @@ package com.antdevrealm.BrainDissecting.web;
 import com.antdevrealm.BrainDissecting.model.dto.ArticleInfoDTO;
 import com.antdevrealm.BrainDissecting.service.ArticleService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/articles")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ArticleRestController {
 
     private final ArticleService articleService;
